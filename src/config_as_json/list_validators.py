4aed220f9a7aef5345263ac104adc9e10d351f50
Tom Björkholm <klausuler_linnet0q@icloud.com>
2026-04-18 14:51:46 +0200

    sketch for more list validators

--- src/config_as_json/list_validators.py
+++ src/config_as_json/list_validators.py
@@ -183,3 +183,138 @@ class ListValueValidator(Validator, Generic[Basictype]):
         msg += 'entire Config object.'
         print(msg, file=stderr_file)
         raise RuntimeError(msg)
+
+
+class ListSizeValidator(Validator):
+    """Validate the size of a list."""
+
+    def __init__(self, min_size: int, max_size: int) -> None:
+        """Initialize the validator.
+
+        Args:
+            min_size: Minimum allowed size of the list.
+            max_size: Maximum allowed size of the list.
+        """
+        self.min_size: int = min_size
+        self.max_size: int = max_size
+
+    def validate_member(self, config: Config,
+                        member_name: str,
+                        member_value: object,
+                        stderr_file: TextIO = sys.stderr) -> Optional[object]:
+        """Validate the size of a list."""
+        _ = config
+        if not isinstance(member_value, list):
+            msg = 'Invalid configuration: '
+            msg += f'Value for {member_name} is not a list.'
+            print(msg, file=stderr_file)
+            raise InvalidConfiguration(msg)
+        if len(member_value) < self.min_size:
+            msg = 'Invalid configuration: '
+            msg += f'Value for {member_name} is list with size '
+            msg += f'{len(member_value)} which is less than minimum '
+            msg += f'{self.min_size}.'
+            print(msg, file=stderr_file)
+            raise InvalidConfiguration(msg)
+        if len(member_value) > self.max_size:
+            msg = 'Invalid configuration: '
+            msg += f'Value for {member_name} is list with size '
+            msg += f'{len(member_value)} which is greater than maximum '
+            msg += f'{self.max_size}.'
+            print(msg, file=stderr_file)
+            raise InvalidConfiguration(msg)
+        return member_value
+
+    def validate(self, config: Config,
+                 stderr_file: TextIO = sys.stderr) -> None:
+        """Raise an exception of incorrect usage.
+
+        Raises:
+            RuntimeError: If the validator is used to validate the entire
+                          Config object.
+        """
+        _ = config
+        msg = 'ListSizeValidator cannot be used to validate the '
+        msg += 'entire Config object.'
+        print(msg, file=stderr_file)
+        raise RuntimeError(msg)
+
+
+class ListIsOrderedValidator(Validator, Generic[Basictype]):
+    """Validate that a list is ordered or only has unique values."""
+
+    def __init__(self,  # pylint: disable=too-many-arguments, too-many-positional-arguments # noqa: E501
+                 element_type: type[Basictype],
+                 is_ordered: bool = True, is_reversed: bool = False,
+                 unique_values: bool = False,
+                 lt_comparator: Callable[[Basictype, Basictype], bool] =
+                 lambda x, y: x < y) -> None:
+        """Initialize the validator.
+        
+        Initializes a validator that checks that a list is ordered
+        or only has unique values. Additionally it always checks that all
+        list elements are of the correct type. If is_ordered is True, the
+        list is checked to be ordered. If is_reversed is True, the list is
+        checked to be reversed. If unique_values is True, the list is
+        checked to only have unique values.
+
+        Args:
+            element_type: The type of the elements in the list. Must be a
+                          one of the Basictype types.
+            is_ordered: Check whether the list is ordered.
+            is_reversed: If True, when checking the order, check that each
+                         element is less than or equal to the previous element.
+                         If False, when checking the order, check that each
+                         element is greater than or equal to the previous
+                         element.
+            unique_values: Check whether the list only has unique values.
+            lt_comparator: Comparator function for the Basictype values.
+                           Defaults to the < operator.
+        """
+        self.element_type: type[Basictype] = element_type
+        self.is_ordered: bool = is_ordered
+        self.is_reversed: bool = is_reversed
+        self.unique_values: bool = unique_values
+        self.lt_comparator: Callable[[Basictype, Basictype], bool] = \
+            lt_comparator
+
+
+class ListOrderingValidator(Validator, Generic[Basictype]):
+    """Validator that reorders a list."""
+
+    def __init__(self,  # pylint: disable=too-many-arguments, too-many-positional-arguments # noqa: E501
+                 element_type: type[Basictype],
+                 order: bool = True, reverse: bool = False,
+                 keep_only_unique: bool = False,
+                 lt_comparator: Callable[[Basictype, Basictype], bool] =
+                 lambda x, y: x < y) -> None:
+        """Initialize the validator.
+
+        Initializes a validator that reorders a list.
+        Additionally it always checks that all list elements are of the correct
+        type.
+
+        Args:
+            element_type: The type of the elements in the list. Must be a
+                          one of the Basictype types.
+            order: If True, the list is reordered to be ordered.
+            reverse: If True, and order is True, the list is reordered
+                     so that each element is less than or equal to the
+                     previous element.
+                     If False, and order is True, the list is reordered
+                     so that each element is greater than or equal
+                     to the previous element.
+                     If True, and order is False, the list is reordered
+                     so that the list is reversed (compared to the order
+                     of the original list).
+            keep_only_unique: If True, the list is reordered to only have
+                              unique values.
+            lt_comparator: Comparator function for the Basictype values.
+                           Defaults to the < operator.
+        """
+        self.element_type: type[Basictype] = element_type
+        self.order: bool = order
+        self.reverse: bool = reverse
+        self.keep_only_unique: bool = keep_only_unique
+        self.lt_comparator: Callable[[Basictype, Basictype], bool] = \
+            lt_comparator
