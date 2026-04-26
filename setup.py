29744f0abcf55b05cabda45ebb3af70f3d482702
Tom Björkholm <klausuler_linnet0q@icloud.com>
2026-04-26 19:42:04 +0200

    0.1 RC

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup  # type: ignore[import-untyped]
 
 setup(
   name='config-as-json',
-  version='0.0.1',
+  version='0.1',
   description='Read, write, validate, and migrate JSON-backed config classes.',
   author='Tom Björkholm',
   author_email='klausuler_linnet0q@icloud.com',
