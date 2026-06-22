53c97b7db51aa866eeb93f17ef4c2f214cb6ce97
Tom Björkholm <klausuler_linnet0q@icloud.com>
2026-06-22 18:04:29 +0200

    1.3 RC

--- setup.py
+++ setup.py
@@ -4,14 +4,10 @@
 from setuptools import setup  # type: ignore[import-untyped]
 
 setup(
-  name='config-as-json', version='1.2.1',
+  name='config-as-json', version='1.3',
   description='Read, write, validate, and migrate JSON-backed config classes.',
   author='Tom Björkholm', author_email='klausuler_linnet0q@icloud.com',
   python_requires='>=3.12', packages=['config_as_json'],
   package_dir={'config_as_json': 'src/config_as_json'},
   package_data={'config_as_json': ['py.typed']},
-  install_requires=[
-    'setuptools >= 82.0.1',
-    'build >= 1.4.2',
-    'wheel >= 0.46.3',
-  ])
+  install_requires=[])
